952bbbb7dfe825803c33a49a282f15b1509ad710
Volodymyr <yushkevich.v2002@gmail.com>
2025-01-31 00:34:32 +0100

    Update of Animal.java

--- src/main/java/com/reserva/natural/model/Animal.java
+++ src/main/java/com/reserva/natural/model/Animal.java
@@ -1,33 +1,103 @@
 package com.reserva.natural.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Column;
-import java.time.LocalDate;
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 
 @Entity
 public class Animal {
 
-   @Id
-   private String nombre;
-   
-   @Column
-   private String tipo;
-   
-   @Column
-   private String familia;
-   
-   @Column
-   private String genero;
-   
-   @Column
-   private String paisOrigen;
-   
-   @Column
-   private LocalDate fechaIngreso;
-   
-   @Column
-   private String imagen;
+    // @Id
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // private Long id;
+    // @JsonProperty("name")
+    // private String name;
+    // @ManyToOne(fetch = FetchType.EAGER)
+    // @JoinColumn(name = "type_id", nullable = false)
+    // @JsonProperty("type")
+    // private AnimalType type;
+    // @ManyToOne(fetch = FetchType.EAGER)
+    // @JoinColumn(name = "family_id", nullable = false)
+    // @JsonProperty("family")
+    // private AnimalFamily family;
+    // @ManyToOne(fetch = FetchType.EAGER)
+    // @JoinColumn(name = "gender_id", nullable = false)
+    // @JsonProperty("gender")
+    // private Gender gender;
+    // @ManyToOne(fetch = FetchType.EAGER)
+    // @JoinColumn(name = "country_id", nullable = false)
+    // @JsonProperty("country")
+    // private Country country;
+    // @JsonProperty("dateOfEntry")
+    // @Temporal(TemporalType.DATE)
+    // private Date dateOfEntry;
 
-   // Getters and Setters
-}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "type_id", nullable = false)
+    private AnimalType type;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "family_id", nullable = false)
+    private AnimalFamily family;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "gender_id", nullable = false)
+    private Gender gender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "country_id", nullable = false)
+    private Country country;
+
+    @Temporal(TemporalType.DATE)
+    private Date dateOfEntry;
+
+
+    private String image;
+
+    public String getName() {
+        return name;
+    }
+
+    public AnimalType getType() {
+        return type;
+    }
+
+    public AnimalFamily getFamily() {
+        return family;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public Date getDateOfEntry() {
+        return dateOfEntry;
+    }
+
+   public String getImage() {
+          return image;
+   }
+  
+  
+      // Getters and setters
+      // Constructor(s)
+  }
