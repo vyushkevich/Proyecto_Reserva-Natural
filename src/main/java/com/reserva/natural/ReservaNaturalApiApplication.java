45f4f595724ca0a313acdae441ad83b8993f81ef
Volodymyr <yushkevich.v2002@gmail.com>
2025-01-30 15:43:03 +0100

    Add dependency for JWT

---
+++ src/main/java/com/reserva/natural/ReservaNaturalApiApplication.java
@@ -0,0 +1,13 @@
+package com.reserva.natural;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaNaturalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaNaturalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reserva/natural/ReservaNaturalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reserva.natural;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaNaturalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
