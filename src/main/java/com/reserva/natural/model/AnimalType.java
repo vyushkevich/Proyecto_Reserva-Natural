5b249ac7c16708e53650297dffd2f9aeb768e9fa
Volodymyr <yushkevich.v2002@gmail.com>
2025-01-31 01:36:18 +0100

    Add new file AnimalType.java to model

---
+++ src/main/java/com/reserva/natural/model/AnimalType.java
@@ -0,0 +1,46 @@
+package com.reserva.natural.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class AnimalType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonProperty("typeName")
+    @Column(nullable = false, unique = true)
+    private String typeName;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "type", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Animal> animals;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public List<Animal> getAnimals() {
+        return animals;
+    }
+
+    // Getters and setters
+    // Constructor(s)
+}
